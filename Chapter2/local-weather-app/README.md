586d9fe4148897779d292dc6c3f5efe27eda78db
Doguhan Uluca <duluca@gmail.com>
2018-05-12 23:42:26 -0400

    cleanup

--- Chapter2/local-weather-app/README.md
+++
@@ -1,7 +0,0 @@
-# LocalCast Weather App
-
-## Web App 
-[README.md](https://github.com/duluca/local-weather-app/blob/master/web-app/README.md)
-
-## Server
-README.md
--- Chapter3/local-weather-app/web-app/src/app/interfaces.ts
+++
@@ -1,8 +0,0 @@
-export interface ICurrentWeather {
-  city: string
-  country: string
-  date: number
-  image: string
-  temperature: number
-  description: string
-}

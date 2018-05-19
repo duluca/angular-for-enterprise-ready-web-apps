674738b331ae537e40c78ee8febda8cbccee6c09
Doguhan Uluca <duluca@gmail.com>
2018-05-19 02:04:39 +0200

    cleaning up imports in ch8

--- Chapter8/lemon-mart/src/app/manager/manager.component.spec.ts
+++ Chapter8/lemon-mart/src/app/manager/manager.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 
 import { ManagerComponent } from './manager.component'
-import { commonTestingModules } from '../common/common.testing';
+import { commonTestingModules } from '../common/common.testing'
 
 describe('ManagerComponent', () => {
   let component: ManagerComponent
